bcb9fd6ef14fcb03633d7c898edeed1beda75c44
qingmeng <qingmeng@live.cn>
2026-01-12 16:31:39 +0000

    add

---
+++ script/merach.js
@@ -0,0 +1,15 @@
+/**
+ * Merach (麦瑞克) 开屏去广告脚本
+ */
+
+let body = $response.body;
+try {
+    let obj = JSON.parse(body);
+    if (obj.data && obj.data.adverts) {
+        obj.data.adverts = [];
+    }
+    body = JSON.stringify(obj);
+} catch (error) {
+    console.log("Merach AdScript Error: " + error);
+}
+$done({ body });
